9fd54c0ac0633b60fe1fefe4ce432ff1109cbf79
Johannes Hofmann <johanneshofmann@keiy.de>
2015-11-06 13:58:32 +0100

    initial import

---
+++ TopCard/src/swt/swl/topcard/MainApp.java
@@ -0,0 +1,73 @@
+package swt.swl.topcard;
+	
+import javafx.application.Application;
+import javafx.stage.Stage;
+import swt.swl.topcard.controller.CardOverviewController;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
+import javafx.fxml.FXMLLoader;
+
+
+public class MainApp extends Application {
+	
+	private Stage primaryStage;
+	private BorderPane rootLayout;
+	
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+			//Initialize primary stage
+			this.primaryStage = primaryStage;
+			this.primaryStage.setTitle("TopCard");
+			
+			//Initialize root layout
+			this.initRootLayout();
+			
+			//Show CardOverview;
+			this.showCardOverview();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+	
+	public void initRootLayout() {
+		try
+		{
+			FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(MainApp.class.getResource("view/RootLayout.fxml"));
+            rootLayout = (BorderPane) loader.load();
+            
+            Scene scene = new Scene(rootLayout);
+            primaryStage.setScene(scene);
+            primaryStage.show();
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+	}
+	
+	public void showCardOverview()
+	{
+		try
+		{
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(MainApp.class.getResource("view/CardOverview.fxml"));
+            AnchorPane cardOverview = (AnchorPane) loader.load();
+
+            rootLayout.setCenter(cardOverview);
+            
+            CardOverviewController controller = loader.getController();
+            //controller.setMainApp(this);
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+	}
+}
---
+++ TopCard/src/swt/swl/topcard/controller/CardOverviewController.java
@@ -0,0 +1,17 @@
+package swt.swl.topcard.controller;
+
+import swt.swl.topcard.MainApp;
+
+
+
+public class CardOverviewController {
+	
+    private MainApp mainApp;
+
+    public CardOverviewController() {
+    }
+    
+    public void setMainApp(MainApp mainApp) {
+        this.mainApp = mainApp;
+    }
+}
