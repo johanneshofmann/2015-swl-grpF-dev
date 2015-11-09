85515dc275461ca997c3bdf5beae5f75ce77235f
Stefan Schmucker <stefanschmucker.stud@uni-bamberg.de>
2015-11-09 18:13:44 +0100

    staged.

--- TopCard/src/swt/swl/topcard/MainApp.java
+++ TopCard/src/swt/swl/topcard/MainApp.java
@@ -1,5 +1,5 @@
 package swt.swl.topcard;
-	
+
 import javafx.application.Application;
 import javafx.stage.Stage;
 import swt.swl.topcard.controller.CardOverviewController;
@@ -10,40 +10,40 @@ import javafx.fxml.FXMLLoader;
 
 
 public class MainApp extends Application {
-	
+	//
 	private Stage primaryStage;
 	private BorderPane rootLayout;
-	
+
 	@Override
 	public void start(Stage primaryStage) {
 		try {
 			//Initialize primary stage
 			this.primaryStage = primaryStage;
 			this.primaryStage.setTitle("TopCard");
-			
+
 			//Initialize root layout
 			this.initRootLayout();
-			
+
 			//Show CardOverview;
 			this.showCardOverview();
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
 	}
-	
+
 	//test
-	
+
 	public static void main(String[] args) {
 		launch(args);
 	}
-	
+
 	public void initRootLayout() {
 		try
 		{
 			FXMLLoader loader = new FXMLLoader();
             loader.setLocation(MainApp.class.getResource("view/RootLayout.fxml"));
             rootLayout = (BorderPane) loader.load();
-            
+
             Scene scene = new Scene(rootLayout);
             primaryStage.setScene(scene);
             primaryStage.show();
@@ -53,19 +53,19 @@ public class MainApp extends Application {
 			e.printStackTrace();
 		}
 	}
-	
+
 	public void showCardOverview()
 	{
 		try
-		
-		
+
+
 		{
             FXMLLoader loader = new FXMLLoader();
             loader.setLocation(MainApp.class.getResource("view/CardOverview.fxml"));
             AnchorPane cardOverview = (AnchorPane) loader.load();
 
             rootLayout.setCenter(cardOverview);
-            
+
             CardOverviewController controller = loader.getController();
             //controller.setMainApp(this);
 		}
