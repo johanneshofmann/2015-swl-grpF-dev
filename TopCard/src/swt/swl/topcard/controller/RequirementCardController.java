4b9f3986b6fbb8b8d2146045476916762366cf54
Moritz <swt-040350@ned.others.swt.wiai.uni-bamberg.de>
2016-01-13 20:07:20 +0100

    views wirklich angepasst

--- TopCard/src/swt/swl/topcard/controller/RequirementCardController.java
+++ TopCard/src/swt/swl/topcard/controller/RequirementCardController.java
@@ -174,7 +174,7 @@ public class RequirementCardController implements Observer {
 		try {
 			FXMLLoader loader = new FXMLLoader();
 			loader.setLocation(getClass().getResource("/swt/swl/topcard/view/SearchRQCardView.fxml"));
-			Pane rootLayout = (Pane) loader.load();
+			ScrollPane rootLayout = (ScrollPane) loader.load();
 			((SearchRQCardController) loader.getController()).setData(this.rqModel, this);
 			Scene scene = new Scene(rootLayout);
 			mainApp.getPrimaryStage().setScene(scene);
@@ -222,7 +222,7 @@ public class RequirementCardController implements Observer {
 		try {
 			FXMLLoader loader = new FXMLLoader();
 			loader.setLocation(getClass().getResource("/swt/swl/topcard/view/ShowAndVoteRQCard.fxml"));
-			Pane rootLayout = (Pane) loader.load();
+			ScrollPane rootLayout = (ScrollPane) loader.load();
 			((ShowAndVoteRqCardController) loader.getController()).setData(this.rqModel, this, rq);
 			Scene scene = new Scene(rootLayout);
 			mainApp.getPrimaryStage().setScene(scene);
